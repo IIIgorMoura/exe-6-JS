e1d750a12cbc057a41bf133d1c14dc8f47e1988d
IIIgorMoura <igor2007moura@gmail.com>
2023-09-18 10:53:37 -0300

    a

--- main.js
+++ main.js
@@ -2,15 +2,26 @@
 let min = 1;
 let max = 100;
 
-let resultado = Math.floor(Math.random() * (max - min + 1) + min);
+let valor = Math.floor(Math.random() * (max - min + 1) + min);
 
-document.querySelector('#resultado').textContent = resultado;
+document.querySelector('#resultado').textContent = valor;
 // fim sorteio valor
 // conferir
+function conferir() {
+    let valorInserido = document.getElementById('chute').value;
 
-// fim conferir
-// chances
-let chances = 5
+    if (valorInserido == valor) {
+        document.getElementById("resposta").innerHTML = "Certa resposta!";
+    } else if(valorInserido < valor) {
+        document.getElementById("resposta").innerHTML = "O valor inserido é MENOR do que o valor sorteado";
+    } else if(valorInserido > valor) {
+        document.getElementById("resposta").innerHTML = "O valor inserido é MAIOR do que o valor sorteado";
+    }
+}
 
+
+    // fim conferir
+    // chances
+    let chances = 5
 
 // fim chances
