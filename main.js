b753bab26479e5f939ba57a04b3d07f967875530
IIIgorMoura <igor2007moura@gmail.com>
2023-09-18 09:54:23 -0300

    a

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-let button = document.getElementById('sortear');
+// sorteio valor
+let min = 1;
+let max = 100;
 
-button.addEventListener('click', function() {
-    let min = 1;
-    let max = 100;
+let resultado = Math.floor(Math.random() * (max - min + 1) + min);
 
-    let resultado = Math.floor(Math.random() * (max - min + 1) + min);
+document.querySelector('#resultado').textContent = resultado;
+// fim sorteio valor
+// conferir
+
+// fim conferir
+// chances
+let chances = 5
 
 
-    document.querySelector('#resultado').textContent = resultado;
-});
+// fim chances
