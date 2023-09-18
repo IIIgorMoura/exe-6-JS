be1ac535f6b294c9faadf3a1150e4911a97da4a1
IIIgorMoura <igor2007moura@gmail.com>
2023-09-18 11:15:33 -0300

    fim java

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let chances = 5
+
 // sorteio valor
 let min = 1;
 let max = 100;
@@ -12,16 +14,18 @@ function conferir() {
 
     if (valorInserido == valor) {
         document.getElementById("resposta").innerHTML = "Certa resposta!";
-    } else if(valorInserido < valor) {
+    } else if (valorInserido < valor) {
+        chances--;
+        document.getElementById("chances").innerHTML = chances;
+
         document.getElementById("resposta").innerHTML = "O valor inserido é MENOR do que o valor sorteado";
-    } else if(valorInserido > valor) {
+    } else if (valorInserido > valor) {
+        chances--;
+        document.getElementById("chances").innerHTML = chances;
+
         document.getElementById("resposta").innerHTML = "O valor inserido é MAIOR do que o valor sorteado";
     }
 }
 
 
-    // fim conferir
-    // chances
-    let chances = 5
-
-// fim chances
+// fim conferir
